ff73614d78fee214c8058ecc0e635aa4b6d9a28f
Peter An <75343007+Exotica0122@users.noreply.github.com>
2026-03-07 22:57:33 +1300

    chore: apply lint and format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-import expressiveCode from 'astro-expressive-code';
-
-import icon from 'astro-icon';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [expressiveCode(), tailwind(), react(), icon()]
+  integrations: [expressiveCode(), tailwind(), react(), icon()],
 });
